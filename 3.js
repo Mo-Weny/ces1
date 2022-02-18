473dce35c752e973dc7aad6d502a3fb6ed5ab5ee
itheima <itheima@itcast.cn>
2022-02-18 22:34:03 +0800

    提交

--- 3.js
+++ 3.js
@@ -6,12 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../111/2.css">
 </head>
 
 <body>
     <h1></h1>
 </body>
-<script src="../111/3.js"></script>
 
 </html>
